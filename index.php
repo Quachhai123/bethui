e5b1062e01927133004411fd1bea68231a6e35fd
Quachhai123 <nguyenquachhai123@gmail.com>
2024-11-12 13:06:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="./style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>BÊ THUI</title>
+</head>
+<body>
+    <div class="container">
+        <header class="d-flex align-items-center justify-content-between pt-3">
+            <div class="col-md-3 mb-2 mb-md-0 d-flex justify-content-start">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="https://dummyimage.com/300" alt="Logo" style="width: 180px; height: 175px;">
+                </a>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center" style="font-size: 20px;">
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="#" class="nav-link text-dark fw-bold me-3 active" aria-current="page">Menu1</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-dark fw-bold me-3">Menu2</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-dark fw-bold me-3">Menu3</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-dark fw-bold me-3">Menu4</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-dark fw-bold me-3">About</a></li>
+                </ul>
+            </div>
+            <div class="d-flex gap-3" style="font-size: 20px;">
+                <i class="fa-brands fa-facebook" style="color: #070ff0;"></i>
+                <i class="fa-brands fa-youtube" style="color: #ff0000;"></i>
+                <i class="fa-brands fa-instagram" style="color: #ba3c26;"></i>
+            </div>
+            <a class="fw-bold text-dark" style="text-decoration: none;">+84 123456789</a>
+        </header>
+        <div class="hero-banner">
+            <a>âjaj</a>
+        </div>
+        <!----------------------->
+        <div class="row">
+            <div class="col-12 text-center my-4">
+                <h1>Sản phẩm</h1>
+            </div>
+        </div>
+        <div>
+            <img src="./img/hero-banner.png" alt="sp" style="width: 310px; height: 240px;">
+        </div>
+
+        <!-- FOOTER -->
+        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-4 my-5" style="border-top: 2px solid #000;">
+            <div class="col mb-3">
+                <a class="d-flex align-items-center mb-3 link-dark">
+                    <img src="./img/hero-banner.png" class="me-2" width="180px" height="175px">
+                </a>
+                <p class="text-muted">© 2024</p>
+            </div>
+
+            <div class="col mb-3"> </div>
+
+            <div class="col mb-3">
+                <h5>Giới Thiệu</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">abc</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">def</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">ghi</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">jkl</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">mno</a></li>
+                </ul>
+            </div>
+
+            <div class="col mb-3">
+                <h5>Danh Mục</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">abc</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">def</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">ghi</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">jkl</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">mno</a></li>
+                </ul>
+            </div>
+
+            <div class="col mb-3">
+                <h5>Liên Hệ</h5>
+                <ul class="nav flex-column">
+                    <li class="d-flex gap-3 py-3" style="font-size: 20px;">
+                        <i class="fa-brands fa-facebook" style="color: #070ff0;"></i>
+                        <i class="fa-brands fa-youtube" style="color: #ff0000;"></i>
+                        <i class="fa-brands fa-instagram" style="color: #ba3c26;"></i>
+                    </li>
+                    <li><a class="text-dark" style="text-decoration: none;">+84 123456789</a></li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+</body>
+</html>
